aa165f5ad14707b7ff6804bcce865812cc4d9c9a
Ariel Coppes <ariel.coppes@gemserk.com>
2012-02-18 19:00:40 -0200

    extracted method to build the toast to a class named Actors used to build common actors

--- prototypes-core/src/main/java/com/gemserk/prototypes/gui/Scene2dToastPrototype.java
+++ prototypes-core/src/main/java/com/gemserk/prototypes/gui/Scene2dToastPrototype.java
@@ -2,17 +2,10 @@ package com.gemserk.prototypes.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Align;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
-import com.gemserk.animation4j.gdx.scenes.scene2d.Scene2dConverters;
-import com.gemserk.animation4j.interpolator.function.InterpolationFunctions;
-import com.gemserk.animation4j.timeline.Builders;
-import com.gemserk.animation4j.timeline.TimelineAnimation;
 import com.gemserk.commons.gdx.GameStateImpl;
+import com.gemserk.commons.gdx.scene2d.Actors;
 import com.gemserk.commons.utils.RandomUtils;
 import com.gemserk.componentsengine.input.InputDevicesMonitorImpl;
 import com.gemserk.componentsengine.input.LibgdxInputMappingBuilder;
@@ -42,63 +35,7 @@ public class Scene2dToastPrototype extends GameStateImpl {
 			"Multiline toasts are also\navailable by adding \\n to the\ntext, however I should learn how to use text wrap.", //
 		};
 
-	Actor toast(final String text, final float time, final Skin skin) {
-
-		return new Window("", skin) {
-
-			TimelineAnimation timelineAnimation;
-
-			{
-				setMovable(false);
-
-				defaults().spaceBottom(5);
-
-				Label toastLabel = new Label(text, skin);
-
-				width = Gdx.graphics.getWidth() * 0.95f;
-				height = toastLabel.getTextBounds().height + 20 + getStyle().titleFont.getLineHeight();
-
-				x = Gdx.graphics.getWidth() * 0.5f - width * 0.5f;
-
-				row().fill().expandX();
-				add(toastLabel).align(Align.LEFT).fill(0f, 0f).padLeft(20);
-
-				float outsideY = Gdx.graphics.getHeight() + height;
-				float insideY = Gdx.graphics.getHeight() - height + getStyle().titleFont.getLineHeight();
-
-				y = outsideY;
-
-				timelineAnimation = Builders.animation( //
-						Builders.timeline() //
-								.value(Builders.timelineValue(this, Scene2dConverters.actorPositionTypeConverter) //
-										.keyFrame(0f, new float[] { x, outsideY }, //
-												InterpolationFunctions.linear(), InterpolationFunctions.easeIn()) //
-										.keyFrame(1f, new float[] { x, insideY }) //
-										.keyFrame(4f, new float[] { x, insideY }, //
-												InterpolationFunctions.linear(), InterpolationFunctions.easeOut()) //
-										.keyFrame(5f, new float[] { x, outsideY }) //
-								) //
-						) //
-						.started(true) //
-						.delay(0f) //
-						.speed(5f / time) //
-						.build();
-			}
-
-			@Override
-			public void act(float delta) {
-				super.act(delta);
-				timelineAnimation.update(delta);
-
-				if (timelineAnimation.isFinished()) {
-					getStage().removeActor(this);
-				}
-			}
-
-		};
-
-	}
-
+	
 	@Override
 	public void init() {
 		gl = Gdx.graphics.getGL10();
@@ -109,7 +46,7 @@ public class Scene2dToastPrototype extends GameStateImpl {
 		// stage = new ToastStage(skin, "This is a toast implementation using scene2d\nwith multiple lines.", Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
 
 		stage = new Stage(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
-		stage.addActor(toast("This is a toast implementation using scene2d\nwith multiple lines.", 3f, skin));
+		stage.addActor(Actors.toast("This is a toast implementation using scene2d\nwith multiple lines.", 3f, skin));
 
 		inputDevicesMonitor = new InputDevicesMonitorImpl<String>();
 		new LibgdxInputMappingBuilder<String>(inputDevicesMonitor, Gdx.input) {
@@ -129,7 +66,7 @@ public class Scene2dToastPrototype extends GameStateImpl {
 		inputDevicesMonitor.update();
 
 		if (inputDevicesMonitor.getButton("toast").isReleased()) {
-			stage.addActor(toast(RandomUtils.random(texts), 3f, skin));
+			stage.addActor(Actors.toast(RandomUtils.random(texts), 3f, skin));
 			// stage = new ToastStage(skin, "This is a toast implementation using scene2d \nwith multiple lines.", Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
 			// Gdx.input.setInputProcessor(stage);
 		}
