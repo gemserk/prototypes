b60077f11a94c83d1fff5d8cb551373df0343607
Ariel Coppes <ariel.coppes@gemserk.com>
2012-02-20 17:04:47 -0200

    changed to use the factory for the two options dialog from commons-gdx project

--- prototypes-core/src/main/java/com/gemserk/prototypes/gui/Scene2dChangelogPrototype.java
+++ prototypes-core/src/main/java/com/gemserk/prototypes/gui/Scene2dChangelogPrototype.java
@@ -2,16 +2,11 @@ package com.gemserk.prototypes.gui;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
-import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Align;
-import com.badlogic.gdx.scenes.scene2d.ui.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.ui.FlickScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Window;
 import com.gemserk.commons.gdx.GameStateImpl;
+import com.gemserk.commons.gdx.scene2d.Actors;
+import com.gemserk.commons.gdx.scene2d.Actors.DialogListener;
 import com.gemserk.highscores.gui.RegisterUserListener;
 import com.gemserk.prototypes.Launcher;
 
@@ -24,59 +19,6 @@ public class Scene2dChangelogPrototype extends GameStateImpl {
 	private Stage stage;
 
 	RegisterUserListener registerUserListener;
-
-	class NotificationListener {
-
-		public void optionSelected(int option) {
-
-		}
-
-	}
-
-	Actor twoOptionsDialog(String[] texts, final NotificationListener notificationListener, String titleText, String firstOption, String secondOption, Skin skin) {
-		Window window = new Window(titleText, skin);
-
-		window.setMovable(false);
-
-		TextButton firstOptionButton = new TextButton(firstOption, skin);
-		TextButton secondOptionButton = new TextButton(secondOption, skin);
-
-		firstOptionButton.setClickListener(new ClickListener() {
-			@Override
-			public void click(Actor actor, float x, float y) {
-				notificationListener.optionSelected(0);
-			}
-		});
-
-		secondOptionButton.setClickListener(new ClickListener() {
-			@Override
-			public void click(Actor actor, float x, float y) {
-				notificationListener.optionSelected(1);
-			}
-		});
-
-		window.defaults().spaceBottom(10);
-		window.row().fill().expandX();
-
-		for (int i = 0; i < texts.length; i++) {
-			window.row().padLeft(20);
-			Label label = new Label(texts[i], skin);
-			window.add(label).align(Align.LEFT).colspan(2);
-		}
-
-		window.row().fill().expandX();
-		window.add(firstOptionButton).align(Align.CENTER).padLeft(20).padRight(20).expandX();
-		window.add(secondOptionButton).align(Align.CENTER).padLeft(20).padRight(20).expandX();
-
-		FlickScrollPane scrollPane = new FlickScrollPane(window);
-
-		scrollPane.width = Gdx.graphics.getWidth() * 0.95f;
-		scrollPane.height = Gdx.graphics.getHeight() * 0.95f;
-		scrollPane.x = Gdx.graphics.getWidth() * 0.5f - scrollPane.width * 0.5f;
-		scrollPane.y = Gdx.graphics.getHeight() * 0.5f - scrollPane.height * 0.5f;
-
-		return scrollPane;
-	}
 
 	@Override
 	public void init() {
@@ -95,7 +37,7 @@ public class Scene2dChangelogPrototype extends GameStateImpl {
 
 		Gdx.input.setInputProcessor(stage);
 
-		stage.addActor(twoOptionsDialog(texts, new NotificationListener() {
+		stage.addActor(Actors.twoOptionsDialog(texts, new DialogListener() {
 
 			@Override
 			public void optionSelected(int option) {
@@ -115,36 +57,6 @@ public class Scene2dChangelogPrototype extends GameStateImpl {
 			}
 
 		}, "New version available", "Update now", "Dismiss", skin));
-
-		// Window window = new Window("New version available", skin);
-		//
-		// window.setMovable(false);
-		//
-		// TextButton yesButton = new TextButton("Update now", skin);
-		// TextButton laterButton = new TextButton("Dismiss", skin);
-		// // TextButton noButton = new TextButton("Never", skin);
-		//
-		// window.defaults().spaceBottom(10);
-		// window.row().fill().expandX();
-		//
-		// for (int i = 0; i < texts.length; i++) {
-		// window.row().padLeft(20);
-		// Label label = new Label(texts[i], skin);
-		// window.add(label).align(Align.LEFT).colspan(2);
-		// }
-		//
-		// window.row().fill().expandX();
-		// window.add(yesButton).align(Align.CENTER).padLeft(20).padRight(20).expandX();
-		// window.add(laterButton).align(Align.CENTER).padLeft(20).padRight(20).expandX();
-		//
-		// FlickScrollPane scrollPane = new FlickScrollPane(window);
-		//
-		// scrollPane.width = Gdx.graphics.getWidth() * 0.95f;
-		// scrollPane.height = Gdx.graphics.getHeight() * 0.95f;
-		// scrollPane.x = Gdx.graphics.getWidth() * 0.5f - scrollPane.width * 0.5f;
-		// scrollPane.y = Gdx.graphics.getHeight() * 0.5f - scrollPane.height * 0.5f;
-		//
-		// stage.addActor(scrollPane);
 
 	}
 
