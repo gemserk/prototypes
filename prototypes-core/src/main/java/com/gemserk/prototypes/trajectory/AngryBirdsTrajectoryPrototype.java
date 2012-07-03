fbc2e4cda98eb8f21b4c34b1895df13e09728971
Ariel Coppes <ariel.coppes@gemserk.com>
2012-07-03 11:53:38 -0300

    added new assets for the angry birds prototype, modified the controller to be similar to angry birds

--- prototypes-core/src/main/java/com/gemserk/prototypes/trajectory/AngryBirdsTrajectoryPrototype.java
+++ prototypes-core/src/main/java/com/gemserk/prototypes/trajectory/AngryBirdsTrajectoryPrototype.java
@@ -9,6 +9,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -43,8 +44,56 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 
 		public float power = 50f;
 		public float angle = 0f;
-		
-		public boolean fixedHorizontalDistance = false; 
+		public boolean charging = false;
+
+		public boolean fixedHorizontalDistance = true;
+
+	}
+
+	public static class ControllerLogic {
+
+		boolean wasPressed;
+		Controller controller;
+
+		final Vector2 pressedPosition = new Vector2();
+		final Vector2 currentPosition = new Vector2();
+		final Vector2 tmp = new Vector2();
+
+		public ControllerLogic(Controller controller, Vector2 slingshotPosition) {
+			this.controller = controller;
+			wasPressed = false;
+			this.pressedPosition.set(slingshotPosition);
+		}
+
+		public void update(float delta) {
+
+			if (Gdx.input.isTouched()) {
+
+				if (!wasPressed) {
+					// pressedPosition.set(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+					wasPressed = true;
+					controller.charging = true;
+				}
+
+				currentPosition.set(Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+
+				tmp.set(currentPosition).sub(pressedPosition);
+				tmp.mul(-1f);
+
+				controller.angle = tmp.angle();
+				controller.power = tmp.len();
+
+			} else {
+
+				if (wasPressed) {
+					// shoot
+					controller.charging = false;
+					wasPressed = false;
+				}
+
+			}
+
+		}
 
 	}
 
@@ -54,7 +103,7 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 		private ProjectileEquation projectileEquation;
 		private Sprite trajectorySprite;
 
-		public int trajectoryPointCount = 30;
+		public int trajectoryPointCount = 50;
 		public float timeSeparation = 1f;
 
 		public TrajectoryActor(Controller controller, float gravity, Sprite trajectorySprite) {
@@ -67,12 +116,19 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 		@Override
 		public void act(float delta) {
 			super.act(delta);
+
+			if (!controller.charging)
+				return;
+
 			projectileEquation.startVelocity.set(controller.power, 0f);
 			projectileEquation.startVelocity.rotate(controller.angle);
 		}
 
 		@Override
 		public void draw(SpriteBatch batch, float parentAlpha) {
+			if (!controller.charging)
+				return;
+
 			float t = 0f;
 			float a = 1f;
 			float adiff = a / trajectoryPointCount;
@@ -82,8 +138,8 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 			float heightDiff = height / trajectoryPointCount;
 
 			float timeSeparation = this.timeSeparation;
-			
-			if (controller.fixedHorizontalDistance) 
+
+			if (controller.fixedHorizontalDistance)
 				timeSeparation = projectileEquation.getTForGivenX(15f);
 
 			for (int i = 0; i < trajectoryPointCount; i++) {
@@ -112,9 +168,19 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 
 	private SpriteBatch spriteBatch;
 	private ShapeRenderer shapeRenderer;
+
 	private Controller controller;
+	private ControllerLogic controllerLogic;
+
 	private Stage stage;
 	private BitmapFont bitmapFont;
+	private Texture targetTexture;
+	private Texture backgroundTexture;
+	private Sprite backgroundSprite;
+	private Texture groundTexture;
+	private Sprite groundSprite;
+	private Sprite slingshotSprite;
+	private Texture slingshotTexture;
 
 	@Override
 	public void init() {
@@ -124,15 +190,42 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 
 		float gravity = -10f;
 
-		Texture texture = new Texture(Gdx.files.internal("test/white-circle.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		Sprite trajectorySprite = new Sprite(texture);
+		Texture.setEnforcePotImages(false);
+
+		targetTexture = new Texture(Gdx.files.internal("test/white-circle.png"));
+		targetTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+		backgroundTexture = new Texture(Gdx.files.internal("angrybirds/background.png"));
+		backgroundTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+		backgroundSprite = new Sprite(backgroundTexture);
+		backgroundSprite.setPosition(0, 0);
+
+		{
+			groundTexture = new Texture(Gdx.files.internal("angrybirds/ground.png"));
+			groundTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+			groundSprite = new Sprite(groundTexture);
+			groundSprite.setPosition(0, 0);
+		}
+
+		{
+			slingshotTexture = new Texture(Gdx.files.internal("angrybirds/slingshot.png"));
+			slingshotTexture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
+
+			slingshotSprite = new Sprite(slingshotTexture);
+			slingshotSprite.setPosition(128 - slingshotSprite.getWidth() * 0.5f, 50);
+		}
+
+		Sprite trajectorySprite = new Sprite(targetTexture);
 
 		controller = new Controller();
+		controllerLogic = new ControllerLogic(controller, new Vector2(128f, 50 + slingshotSprite.getHeight() * 0.7f));
+
 		TrajectoryActor trajectoryActor = new TrajectoryActor(controller, gravity, trajectorySprite);
 
-		trajectoryActor.x = 100f;
-		trajectoryActor.y = 100f;
+		trajectoryActor.x = 128f;
+		trajectoryActor.y = 50 + slingshotSprite.getHeight() * 0.7f;
 		trajectoryActor.width = 10f;
 		trajectoryActor.height = 10f;
 
@@ -146,28 +239,55 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 		Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1f);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 
-		float deltaTime = Gdx.graphics.getDeltaTime();
+		float delta = Gdx.graphics.getDeltaTime();
 
-		stage.act(deltaTime);
-		stage.draw();
-		
+		controllerLogic.update(delta);
+
 		spriteBatch.begin();
-		bitmapFont.draw(spriteBatch, "LEFT and RIGHT keys to change the start velocity", Gdx.graphics.getWidth() * 0.2f, Gdx.graphics.getHeight() * 0.9f);
-		bitmapFont.draw(spriteBatch, "UP and DOWN keys to change the angle", Gdx.graphics.getWidth() * 0.2f, Gdx.graphics.getHeight() * 0.85f);
+		backgroundSprite.draw(spriteBatch);
+		groundSprite.draw(spriteBatch);
+		bitmapFont.draw(spriteBatch, "Touch over the slingshot and drag to modify the trajectory", Gdx.graphics.getWidth() * 0.2f, Gdx.graphics.getHeight() * 0.9f);
 		bitmapFont.draw(spriteBatch, "5 and 6 to switch fixed horizontal distance between points (" + controller.fixedHorizontalDistance + ")", Gdx.graphics.getWidth() * 0.2f, Gdx.graphics.getHeight() * 0.8f);
 		spriteBatch.end();
-
-		if (Gdx.input.isKeyPressed(Keys.UP)) {
-			controller.angle += 15f * Gdx.graphics.getDeltaTime();
-			if (controller.angle > 80f)
-				controller.angle = -30f;
+		
+		if (controller.charging) {
+			shapeRenderer.begin(ShapeType.FilledTriangle);
+			shapeRenderer.setColor(0.2f, 0f, 0f, 1f);
+			// shapeRenderer.filledTriangle(50f, 50f, 55f, 55f, 60f, 60f);
+			shapeRenderer.filledTriangle(120f, 124f, 120f, 130f, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+			shapeRenderer.filledTriangle(140f, 144f, 140f, 150f, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+			shapeRenderer.end();
+		} else {
+			shapeRenderer.begin(ShapeType.FilledTriangle);
+			shapeRenderer.setColor(0.2f, 0f, 0f, 1f);
+			// shapeRenderer.filledTriangle(50f, 50f, 55f, 55f, 60f, 60f);
+//			shapeRenderer.filledTriangle(120f, 124f, 120f, 130f, Gdx.input.getX(), Gdx.graphics.getHeight() - Gdx.input.getY());
+			shapeRenderer.filledTriangle(140f, 144f, 140f, 150f, 120f, 124f);
+			shapeRenderer.filledTriangle(120f, 124f, 120f, 130f, 140f, 150f);
+			shapeRenderer.end();
+			
 		}
+		
+		spriteBatch.begin();
+		slingshotSprite.draw(spriteBatch);
+		spriteBatch.end();
 
-		if (Gdx.input.isKeyPressed(Keys.DOWN)) {
-			controller.angle -= 10f * Gdx.graphics.getDeltaTime();
-			if (controller.angle < -30f)
-				controller.angle = 80f;
-		}
+		stage.act(delta);
+		stage.draw();
+
+
+
+		// if (Gdx.input.isKeyPressed(Keys.UP)) {
+		// controller.angle += 15f * Gdx.graphics.getDeltaTime();
+		// if (controller.angle > 80f)
+		// controller.angle = -30f;
+		// }
+		//
+		// if (Gdx.input.isKeyPressed(Keys.DOWN)) {
+		// controller.angle -= 10f * Gdx.graphics.getDeltaTime();
+		// if (controller.angle < -30f)
+		// controller.angle = 80f;
+		// }
 
 		if (Gdx.input.isKeyPressed(Keys.RIGHT)) {
 			controller.power += 10f * Gdx.graphics.getDeltaTime();
@@ -176,7 +296,7 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 		if (Gdx.input.isKeyPressed(Keys.LEFT)) {
 			controller.power -= 10f * Gdx.graphics.getDeltaTime();
 		}
-		
+
 		if (Gdx.input.isKeyPressed(Keys.NUM_5)) {
 			controller.fixedHorizontalDistance = false;
 		}
@@ -193,6 +313,10 @@ public class AngryBirdsTrajectoryPrototype extends GameStateImpl {
 		shapeRenderer.dispose();
 		stage.dispose();
 		bitmapFont.dispose();
+		targetTexture.dispose();
+		backgroundTexture.dispose();
+		groundTexture.dispose();
+		slingshotTexture.dispose();
 	}
 
 }
